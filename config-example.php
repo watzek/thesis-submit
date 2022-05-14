7a9cb6d8d21fa181aaeaf7b6dfc29ac6341e6ab6
jeremymcwilliams <jeremym@lclark.edu>
2022-05-14 20:19:37 +0000

    gitignore and example config

---
+++ config-example.php
@@ -0,0 +1,11 @@
+<?php
+
+/* this file contains Omeka API key, google auth credentials, maybe db creds if needed */
+/* in production, this is renamed config.php (and listed in .gitignore) */
+
+$omekaApiKey="apikeyvalue";
+
+
+
+
+ ?>
