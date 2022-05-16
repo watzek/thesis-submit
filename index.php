ea0a6be824cfcdb41eba4bc23984ee0a1e086fc1
jeremymcwilliams <jeremym@lclark.edu>
2022-05-16 21:56:21 +0000

    just testing this commit using issue-based branching/committing

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
     <h1>Hello, world!</h1>
 
     <!-- Optional JavaScript; choose one of the two! -->
-
     <!-- Option 1: Bootstrap Bundle with Popper -->
+    <!-- is this working????? -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   </body>
