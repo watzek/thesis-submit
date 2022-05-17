ac33e16b7a44175f8dbb25e3ec26f1aab5c9d03a
Sam Flores <97924281+sam-flores@users.noreply.github.com>
2022-05-17 11:09:27 -0700

    This wasn't meant to be there...

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
- $( window ).on( "load", function() {
-   $('#flexSwitchCheckChecked').on('change.bootstrapSwitch', function(e) {
-       // console.log(e.target.checked);
-       if(e.target.checked == true) $('#delayedReleaseDateDiv').removeClass('d-none').addClass('d-flex');
-       if(e.target.checked == false) $('#delayedReleaseDateDiv').addClass('d-none').removeClass('d-flex');
-   });
- });
